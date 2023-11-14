015ac7f87551981e4b2e64406085136a38df7256
daria-koivisto <daria.koivisto@tktk.ee>
2023-11-14 09:55:59 +0200

    week6

---
+++ Week6Arrays/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] fruitBasket = { "apples", "bananas", "oranges" };
+Console.WriteLine($"There are {fruitBasket.Length} fruits in your basket");
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
+fruitBasket[0] = "pineapples";
+fruitBasket[2] = "peaches";
+
+for (int i = 0;  i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,12 @@
+﻿string userName = "Daria";
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}");
+//int counter = 0;
+//for(int i = 0; i < userName.Length; i++)
+//{
+//    if (userName[i] == 'a')
+//    {  counter++; }
+//}
+//Console.WriteLine($"Your name has {counter} a-letters");
+string updatedUserName = userName.Replace('a', '4').Replace('r', '3');
+Console.WriteLine(updatedUserName);
